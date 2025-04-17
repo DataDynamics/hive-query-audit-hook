a87fae9f8da7a9c02237eea3e0d99a3d0bc999d6
김병곤 <fharenheit@gmail.com>
2025-04-18 08:07:33 +0900

    주석 추가

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Requirement
 
-* Hive 3.1.3
+* Hive 3.1.3 (Cloudera CDP 7.1.9 SP1)
 * JDK 8
 
 ## Build
@@ -61,3 +61,27 @@
 * Target URL
   * Name : `QueryAuditHook.target.url` 
   * Value : `http://10.10.10.10/api/hive/audit`
+
+## Deployment
+
+* 빌드한 JAR 파일은 `hive-site.xml` 파일에  `hive.aux.jars.path` 설정을 통해 지정 가능합니다.
+* Comma Separated로 N개 JAR 파일을 지정할 수 있습니다.
+* Hook에서 외부 라이브러리를 사용하는 경우 Maven dependency plugin을 통해 함께 패키징하도록 합니다.
+* JAR 파일 지정시 경로는 로컬 파일 시스템 또는 HDFS 지정할 수 있습니다.
+* 로컬 파일 시스템을 지정하는 경우 모든 노드에 배포하도록 하도록 합니다.
+
+### Valina Hadoop
+
+`hive-site.xml` 파일에 다음을 추가합니다.
+
+```xml
+<property>
+  <name>hive.aux.jars.path</name>
+  <value>hdfs:///user/hive/hooks/my-hive-hook-1.0.jar,file:///opt/custom_hive_jars/another-library.jar</value>
+  <description>HDFS 및 로컬 파일 시스템의 보조 JAR 경로</description>
+</property>
+```
+
+### Cloudera CDP
+
+"Cloudera Manager > Hive On Tez > Configuration > Hive Auxiliary JARs Directory"를 찾아서 경로를 지정하도록 합니다.
--- src/main/java/io/datadynamics/hive/hook/HttpRequestHelper.java
+++ src/main/java/io/datadynamics/hive/hook/HttpRequestHelper.java
@@ -11,6 +11,12 @@ import java.net.http.HttpRequest.BodyPublisher;
 import java.net.http.HttpRequest.Builder;
 import java.net.http.HttpResponse;
 
+/**
+ * HTTP 요청을 처리하기 위한 Helper 클래스.
+ * RESTful API 호출을 위한 GET, POST, PUT, PATCH, DELETE 메소드를 제공합니다.
+ *
+ * @author KIM BYOUNG GON (support@data-dynamics.io)
+ */
 public class HttpRequestHelper {
 
     private final String APPLICATION_JSON = "application/json";
@@ -22,51 +28,129 @@ public class HttpRequestHelper {
 
     private final HttpClient httpClient;
 
+    /**
+     * HttpRequestHelper 생성자
+     *
+     * @param httpClient HTTP 요청을 수행할 HttpClient 인스턴스
+     */
     public HttpRequestHelper(final HttpClient httpClient) {
         this.httpClient = httpClient;
     }
 
-    public <T> T get(String url, String token, Class<T> valueType)
-            throws IOException, InterruptedException {
+    /**
+     * GET 요청을 수행합니다.
+     *
+     * @param url       요청할 URL
+     * @param token     인증 토큰 (없는 경우 null)
+     * @param valueType 응답 결과를 변환할 클래스 타입
+     * @return 지정된 타입으로 변환된 응답 결과
+     * @throws IOException          IO 예외 발생시
+     * @throws InterruptedException 요청이 중단된 경우
+     */
+    public <T> T get(String url, String token, Class<T> valueType) throws IOException, InterruptedException {
         Builder builder = HttpRequest.newBuilder().GET().uri(URI.create(url));
         return send(valueType, token, builder);
     }
 
+    /**
+     * 토큰 없이 POST 요청을 수행합니다.
+     *
+     * @param uri       요청할 URI
+     * @param request   요청 본문 객체
+     * @param valueType 응답 결과를 변환할 클래스 타입
+     * @return 지정된 타입으로 변환된 응답 결과
+     * @throws IOException          IO 예외 발생시
+     * @throws InterruptedException 요청이 중단된 경우
+     */
     public <T> T post(String uri, Object request, Class<T> valueType)
             throws IOException, InterruptedException {
         return post(uri, request, valueType, null);
     }
 
-    public <T> T post(String uri, Object request, Class<T> valueType, String token)
-            throws IOException, InterruptedException {
+    /**
+     * 토큰과 함께 POST 요청을 수행합니다.
+     *
+     * @param uri       요청할 URI
+     * @param request   요청 본문 객체
+     * @param valueType 응답 결과를 변환할 클래스 타입
+     * @param token     인증 토큰 (없는 경우 null)
+     * @return 지정된 타입으로 변환된 응답 결과
+     * @throws IOException          IO 예외 발생시
+     * @throws InterruptedException 요청이 중단된 경우
+     */
+    public <T> T post(String uri, Object request, Class<T> valueType, String token) throws IOException, InterruptedException {
         Builder builder = HttpRequest.newBuilder().uri(URI.create(uri)).POST(getBodyPublisher(request)).header(CONTENT_TYPE, APPLICATION_JSON);
         return send(valueType, token, builder);
     }
 
-    public <T> T put(String uri, Object request, Class<T> valueType, String token)
-            throws IOException, InterruptedException {
+    /**
+     * PUT 요청을 수행합니다.
+     *
+     * @param uri       요청할 URI
+     * @param request   요청 본문 객체
+     * @param valueType 응답 결과를 변환할 클래스 타입
+     * @param token     인증 토큰 (없는 경우 null)
+     * @return 지정된 타입으로 변환된 응답 결과
+     * @throws IOException          IO 예외 발생시
+     * @throws InterruptedException 요청이 중단된 경우
+     */
+    public <T> T put(String uri, Object request, Class<T> valueType, String token) throws IOException, InterruptedException {
         Builder builder = HttpRequest.newBuilder().uri(URI.create(uri)).PUT(getBodyPublisher(request)).header(CONTENT_TYPE, APPLICATION_JSON);
         return send(valueType, token, builder);
     }
 
-    public <T> T patch(String uri, Class<T> valueType, String token)
-            throws IOException, InterruptedException {
+    /**
+     * PATCH 요청을 수행합니다.
+     *
+     * @param uri       요청할 URI
+     * @param valueType 응답 결과를 변환할 클래스 타입
+     * @param token     인증 토큰 (없는 경우 null)
+     * @return 지정된 타입으로 변환된 응답 결과
+     * @throws IOException          IO 예외 발생시
+     * @throws InterruptedException 요청이 중단된 경우
+     */
+    public <T> T patch(String uri, Class<T> valueType, String token) throws IOException, InterruptedException {
         Builder builder = HttpRequest.newBuilder().uri(URI.create(uri)).method("PATCH", HttpRequest.BodyPublishers.noBody()).header(CONTENT_TYPE, APPLICATION_JSON);
         return send(valueType, token, builder);
     }
 
-    public <T> T delete(String uri, Class<T> valueType, String token)
-            throws IOException, InterruptedException {
+    /**
+     * DELETE 요청을 수행합니다.
+     *
+     * @param uri       요청할 URI
+     * @param valueType 응답 결과를 변환할 클래스 타입
+     * @param token     인증 토큰 (없는 경우 null)
+     * @return 지정된 타입으로 변환된 응답 결과
+     * @throws IOException          IO 예외 발생시
+     * @throws InterruptedException 요청이 중단된 경우
+     */
+    public <T> T delete(String uri, Class<T> valueType, String token) throws IOException, InterruptedException {
         Builder builder = HttpRequest.newBuilder().uri(URI.create(uri)).DELETE();
         return send(valueType, token, builder);
     }
 
+    /**
+     * 요청 본문을 BodyPublisher로 변환합니다.
+     *
+     * @param request 변환할 요청 객체
+     * @return 변환된 BodyPublisher
+     * @throws JsonProcessingException JSON 변환 실패시
+     */
     private BodyPublisher getBodyPublisher(Object request) throws JsonProcessingException {
         return HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(request));
     }
 
-    private <T> T send(Class<T> valueType, String token, Builder builder)
-            throws IOException, InterruptedException {
+    /**
+     * HTTP 요청을 실행하고 응답을 처리합니다.
+     *
+     * @param valueType 응답 결과를 변환할 클래스 타입
+     * @param token     인증 토큰 (없는 경우 null)
+     * @param builder   HTTP 요청 빌더
+     * @return 지정된 타입으로 변환된 응답 결과
+     * @throws IOException          IO 예외 발생시
+     * @throws InterruptedException 요청이 중단된 경우
+     */
+    private <T> T send(Class<T> valueType, String token, Builder builder) throws IOException, InterruptedException {
         if (token != null) {
             builder.header(AUTHORIZATION, BEARER + token);
         }
